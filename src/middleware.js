89460ce2c3ded7d79651692abd2d032cafd4d9a6
Afnan Sayed <afnansayed1973@gimal.com>
2024-08-26 21:56:00 +0600

    check user and cookies with middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,10 +3,51 @@ import { NextResponse } from "next/server"
 
 
 
+// export const  middleware = (request) => {
+//        return NextResponse.redirect(new URL('/dashboard',request.url))
+// }
+
+// export const config = {
+//     matcher : ['/About','/contact']
+// }
+
+// try with out matcher 
+// export const  middleware = (request) => {
+//     if(request.nextUrl.pathname.startsWith('/About'))
+//     {
+//         return NextResponse.redirect(new URL('/dashboard',request.url)) 
+//     }   
+// }
+
+
+// user check basic  
+//const user = true;
+// export const  middleware = (request) => {
+//     if(!user)
+//     {
+//         return NextResponse.redirect(new URL('/About',request.url)) 
+//     }  
+//     return NextResponse.next(); 
+// }
+
+// export const config = {
+//     matcher : ['/Blogs','/dashboard']
+// }
+
+
+// check cookies and redirect to login
+const coo = 'next-superhero';
 export const  middleware = (request) => {
-       return NextResponse.redirect(new URL('/dashboard',request.url))
+    const cookies = request.cookies.get('token');
+    //console.log(request.cookies.get('token'));
+
+    if(!cookies || cookies.value !== coo)
+    {
+        return NextResponse.redirect(new URL('/About',request.url)) 
+    }  
+    return NextResponse.next(); 
 }
 
 export const config = {
-    matcher : ['/About','/contact']
+    matcher : ['/Blogs','/dashboard']
 }
