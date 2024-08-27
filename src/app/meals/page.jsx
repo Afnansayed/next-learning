622a60fe7c2d13350878c091bff1870a5e5e92c0
Afnan Sayed <afnansayed1973@gimal.com>
2024-08-28 00:14:41 +0600

    module css add

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,11 +1,11 @@
 import Meals from '@/components/Meals/Meals';
 import React from 'react';
-
+import styles from './styles.module.css'
 const mealsPage = () => {
     return (
         <div>
              <h1 className='text-3xl'>Meals Page  which is coming soon</h1>
-             <h2 className='text-xl'>Meals are comming soon .......</h2>
+             <h2 className={styles.text_size}>Meals are comming soon .......</h2>
              <Meals/>
         </div>
     );
