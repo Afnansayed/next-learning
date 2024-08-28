be504cb08ee1382132cb72f73ca4eb3a7320e180
Afnan Sayed <afnansayed1973@gimal.com>
2024-08-28 22:00:11 +0600

    catching and revalidting

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+
+const getTime = async () => {
+    const res = await fetch('http://localhost:3000/time', {next:  {revalidate: 10}});
+    const data = await res.json();
+    return data.currentTime;
+}
 const page = () => {
    // console.log (name)
+   const time = getTime();
     return (
         <div>
-            about 
+           <h3>About our company and learn more....</h3>
+           <h3>Time : {time} </h3>
         </div>
     );
 };
